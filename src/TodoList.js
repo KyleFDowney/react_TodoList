07882362c4a97059262bcefe505ff60297d45288
capntechbeard <capntechbeard@users.noreply.github.com>
2018-09-05 19:04:57 -0500

    1st

---
+++ src/TodoList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(e) {
+    alert('Daboo');
+  }
+
+  render () {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input placeholder="enter task">
+            </input>
+            <button type="submit">add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
