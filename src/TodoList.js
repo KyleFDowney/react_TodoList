9b8ba4d65931a2baf88a9e96dca1e5211c55570e
capntechbeard <capntechbeard@users.noreply.github.com>
2018-09-09 15:15:06 -0500

    finished functioning list

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
+import TodoItems from "./TodoItems";
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      items: []
+    };
+
     this.addItem = this.addItem.bind(this);
   }
 
   addItem(e) {
-    alert('Daboo');
+    if (this._inputElement.value !== '') {
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+
+      this.setState((prevState) => {
+        return {
+          items: prevState.items.concat(newItem)
+        };
+      });
+
+      this._inputElement.value = '';
+    }
+
+    console.log(this.state.items);
+
+    e.preventDefault();
   }
 
   render () {
@@ -16,11 +38,13 @@ class TodoList extends Component {
       <div className="todoListMain">
         <div className="header">
           <form onSubmit={this.addItem}>
-            <input placeholder="enter task">
+            <input ref={(a) => this._inputElement = a}
+                    placeholder="enter task">
             </input>
             <button type="submit">add</button>
           </form>
         </div>
+        <TodoItems entries={this.state.items}/>
       </div>
     );
   }
